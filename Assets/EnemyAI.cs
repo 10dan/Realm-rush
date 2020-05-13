2ca6af049c6fc2a268001c28ad1ecc7591dae4c4
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-05-13 15:20:58 +0100

    Added coordinate system

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    [SerializeField] List<Block> path;
+    void Start() {
+        foreach(Block b in path){
+            print(b.name);
+        }
+    }
+    void Update() {
+
+    }
+}

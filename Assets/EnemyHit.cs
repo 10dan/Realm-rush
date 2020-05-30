b74faf6d8fdc8e870b307b1708ca034e96b3bbdf
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-05-30 11:16:50 +0100

    towers shoot

---
+++ Assets/EnemyHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    [SerializeField] int hp = 10;
+    private void OnParticleCollision(GameObject other) {
+        ProcessHit();
+        if(hp <= 0) {
+            KillEnemy();
+        }
+    }
+    void ProcessHit() {
+        hp -= 1;
+        print("Hp remaining: " + hp);
+    }
+
+    void KillEnemy() {
+        Destroy(gameObject);
+    }
+}
